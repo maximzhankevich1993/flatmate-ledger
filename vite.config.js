3adc327b95d3a7141849e2039e265c35cc2b8007
maximzhankevich1993 <h6pbsvx5dg@privaterelay.appleid.com>
2026-06-09 10:47:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/plugins';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			// Разрешаем Vite читать файлы выше по дереву, если нужно
+			allow: ['.']
+		}
+	}
 });
